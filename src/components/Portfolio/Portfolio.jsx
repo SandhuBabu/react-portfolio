0a0187ff14e246f16c0ee28dd8645391d61292db
Sandhu Babu <53761189+SandhuBabu@users.noreply.github.com>
2023-10-31 10:04:51 +0530

    Update Portfolio.jsx

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -6,42 +6,42 @@ const Portfolio = () => {
     {
       id: 1,
       image: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2e948896757753.5eb58c727d672.png',
-      title: 'Music Player Android App',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     },
     {
       id: 2,
       image: 'https://cdn.dribbble.com/users/2193891/screenshots/9535419/media/ac2bfbe71a2680a8c893bd6ed4de8ee5.png',
-      title: 'Office Management System',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     },
     {
       id: 3,
       image: 'https://cdn.dribbble.com/users/748883/screenshots/6605184/ecommerce_mobile_app_2x.png',
-      title: 'E-Commerce Android App',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     },
     {
       id: 4,
       image: 'https://cdn.dribbble.com/users/1820876/screenshots/11118758/media/c39d44230f53c45ff1b132290e3e3650.png',
-      title: 'Messaging Android App',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     },
     {
       id: 5,
       image: 'https://i.pinimg.com/originals/a3/8a/c6/a38ac697e770aaa0dd88492238612d7e.png',
-      title: 'Ether Wallet',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     },
     {
       id: 6,
       image: 'https://i.pinimg.com/originals/48/03/9c/48039ce7a6586d1205d9a58721fb707b.png',
-      title: 'Video Chat Android App',
+      title: 'Lorem ipsum dolor sit',
       github: 'https://github.com',
       demo: 'https://google.com'
     }
